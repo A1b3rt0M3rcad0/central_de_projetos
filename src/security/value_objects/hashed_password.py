88111ba9fc34ee21337e23e87646853057efab32
Alberto <alberto.zimba2013@gmail.com>
2025-04-10 15:52:39 -0300

    feat: hashed_password created and blowfish_crypt created

---
+++ src/security/value_objects/hashed_password.py
@@ -0,0 +1,29 @@
+from src.security.value_objects.interface.i_hashed_password import IHashedPassword
+from src.domain.value_objects.password import Password
+from src.security.cryptography.interface.i_cryptography import ICryptography
+from src.security.cryptography.interface.i_salt import ISalt
+
+class HashedPassword(IHashedPassword):
+
+    def __init__(self, password: Password, cryptography: ICryptography, salt: ISalt) -> None:
+        self.__salt = salt
+        self.__cryptography = cryptography
+        self.__hashed_password = self.__cryptography.hash(password, self.__salt)
+
+    @property
+    def hashed_password(self) -> bytes:
+        return self.__hashed_password
+    
+    @property
+    def salt(self) -> ISalt:
+        return self.__salt.salt
+    
+    @property
+    def cryptography(self) -> ICryptography:
+        return self.__cryptography
+    
+    def __str__(self) -> str:
+        return self.hashed_password.decode()
+    
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self.hashed_password.decode()})'
