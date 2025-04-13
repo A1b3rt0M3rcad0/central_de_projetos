f0ca2c238ab7ddcc6c146a88db8df2960c621c4f
Alberto <alberto.zimba2013@gmail.com>
2025-04-13 12:57:10 -0300

    feat: models created

--- src/infra/models/user.py
+++ src/infra/models/user.py
@@ -1,8 +1,10 @@
 from sqlalchemy import String, Column, DateTime, LargeBinary
 from src.infra.config.base.base_model import BaseModel
 from datetime import datetime, timezone
+from sqlalchemy.orm import relationship, backref
 
 class User(BaseModel):
+    
     __tablename__ = 'user'
 
     cpf = Column(String(15), primary_key=True)
@@ -11,5 +13,4 @@ class User(BaseModel):
     email = Column(String(50), nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime(timezone.utc))
 
-    def __repr__(self):
-        return f"<User(cpf='{self.cpf}', email='{self.email}', role='{self.role}')>"
+    user_project = relationship('UserProject', backref=backref('user'))
---
+++ src/infra/models/user_project.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, DateTime, ForeignKey
+from src.infra.config.base.base_model import BaseModel
+from datetime import datetime, timezone
+
+class UserProject(BaseModel):
+
+    __tablename__ = 'user_project'
+
+    user_cpf = Column(ForeignKey('user.cpf'), nullable=False, primary_key=True)
+    project_id = Column(ForeignKey('project.id'), nullable=False, primary_key=True)
+    assignment_date = Column(DateTime, default=datetime(timezone.utc), nullable=False)
