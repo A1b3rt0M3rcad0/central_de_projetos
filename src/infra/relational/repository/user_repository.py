5baaac0f99a64c744c61ea7836c69d6614c6bdec
Alberto <alberto.zimba2013@gmail.com>
2025-04-14 15:33:17 -0300

    feat: created find method in user_repository

--- src/infra/relational/repository/user_repository.py
+++ src/infra/relational/repository/user_repository.py
@@ -5,7 +5,9 @@ from src.security.cryptography.interface.i_salt import ISalt
 from src.domain.value_objects.email import Email
 from src.domain.value_objects.roles import Role
 from src.infra.relational.config.interface.i_db_connection_handler import IDBConnectionHandler
+from src.domain.entities.user import UserEntity
 from src.infra.relational.models.user import User
+from typing import Optional
 
 class UserRepository(IUserRepository):
 
@@ -29,8 +31,24 @@ class UserRepository(IUserRepository):
             except Exception as e:
                 raise e
 
-    def find(self, cpf):
-        return None
+    def find(self, cpf:CPF) -> Optional[UserEntity]:
+        cpf_entry = cpf.value
+
+        with self.__db_connection_handler as db:
+            try:
+                result = db.session.query(User).where(User.cpf == cpf_entry).first()
+                if result:
+                    return UserEntity(
+                        cpf=CPF(result.cpf),
+                        password=result.password, # bytes
+                        salt=result.salt, # bytes
+                        role=Role(result.role),
+                        email=Email(result.email),
+                        created_at=result.created_at
+                    )
+                return None 
+            except Exception as e:
+                raise e
 
     def update(self, cpf, update_params):
         return None
