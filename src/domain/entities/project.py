a37837bb7f80fab738b709d99f4b7141bfa6c6a9
Alberto <alberto.zimba2013@gmail.com>
2025-04-15 14:20:31 -0300

    feat: find method in project_repository created and test

--- src/domain/entities/project.py
+++ src/domain/entities/project.py
@@ -3,12 +3,18 @@ from datetime import datetime
 
 class ProjectEntity:
 
-    def __init__(self, project_id:int, status_id:int, description:str, verba_disponivel:MonetaryValue, andamento_projeto:str, data_inicio:datetime, data_final_prevista:datetime, data_finalizacao:datetime) -> None:
+    def __init__(self, 
+                 project_id:int, 
+                 status_id:int, 
+                 verba_disponivel:MonetaryValue, 
+                 andamento_do_projeto:str, 
+                 start_date:datetime, 
+                 expected_completion_date:datetime, 
+                 end_date:datetime) -> None:
         self.project_id = project_id
         self.status_id = status_id
-        self.description = description
         self.verba_disponivel = verba_disponivel
-        self.andamento_projeto = andamento_projeto
-        self.data_inicio = data_inicio
-        self.data_final_prevista = data_final_prevista
-        self.data_finalizacao = data_finalizacao
+        self.andamento_do_projeto = andamento_do_projeto
+        self.start_date = start_date
+        self.expected_completion_date = expected_completion_date
+        self.end_date = end_date
