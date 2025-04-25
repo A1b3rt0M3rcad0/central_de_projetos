d9ff55de06c660d79c954e48a3a5823a0f318236
Alberto <alberto.zimba2013@gmail.com>
2025-04-25 14:20:14 -0300

    feat: update project expected completion date case create

---
+++ src/data/use_cases/update_project_expected_completion_date_entry.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from src.data.interface.i_project_repository import IProjectRepository
+from src.domain.use_cases.i_update_project_expected_completion_date_entry import IUpdateProjectExpectedCompletionDate
+
+class UpdateProjectExpectedCompletionDate(IUpdateProjectExpectedCompletionDate):
+
+    def __init__(self, project_repository:IProjectRepository) -> None:
+        self.__project_repository = project_repository
+    
+    def update(self, project_id:int, expected_completion_date:datetime) -> None:
+        try:
+            update_params = {'expected_completion_date':expected_completion_date}
+            self.__project_repository.update(
+                project_id=project_id,
+                expected_completion_date=update_params
+            )
+        except Exception as e:
+            raise e from e
