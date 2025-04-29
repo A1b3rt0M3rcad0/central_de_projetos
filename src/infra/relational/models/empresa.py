3c82fc96c0534ad48782c94c31e0e9cfbe630e27
Alberto <alberto.zimba2013@gmail.com>
2025-04-29 18:00:06 -0300

    feat: empresa model created

--- src/infra/relational/models/empresa.py
+++ src/infra/relational/models/empresa.py
@@ -1,6 +1,8 @@
 from sqlalchemy import String, DateTime, Column, Integer
 from src.infra.relational.config.base.base_model import BaseModel
 from datetime import datetime, timezone
+from src.infra.relational.models.project_empresa import ProjectEmpresa
+from sqlalchemy.orm import backref, relationship
 
 class Empresa(BaseModel):
 
@@ -8,3 +10,5 @@ class Empresa(BaseModel):
     id = Column(Integer, primary_key=True)
     name = Column(String(100), unique=True)
     created_at = Column(DateTime, default=datetime.now(timezone.utc))
+
+    project_empresa = relationship(ProjectEmpresa, backref=backref('empresa'))
