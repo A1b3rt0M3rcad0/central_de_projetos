6519ad8a6865fc9649f7b1d4d4e3c76a2ec00060
Alberto <alberto.zimba2013@gmail.com>
2025-04-30 15:20:54 -0300

    feat: project_types model and entity created

--- src/infra/relational/models/types.py
+++ src/infra/relational/models/types.py
@@ -1,6 +1,8 @@
 from sqlalchemy import String, DateTime, Column, Integer
 from src.infra.relational.config.base.base_model import BaseModel
 from datetime import datetime, timezone
+from src.infra.relational.models.project_types import ProjectTypes
+from sqlalchemy.orm import backref, relationship
 
 class Types(BaseModel):
 
@@ -9,3 +11,5 @@ class Types(BaseModel):
     id = Column(Integer, primary_key=True)
     name = Column(String(100), unique=True)
     created_at = Column(DateTime, default=datetime.now(timezone.utc))
+
+    project_types = relationship(ProjectTypes, backref=backref('types'))
