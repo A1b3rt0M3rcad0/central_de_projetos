01f21873655bd63e5ce721bdcae6028643220299
Alberto <alberto.zimba2013@gmail.com>
2025-05-07 13:41:39 -0300

    feat: get_document_names_controller crated

---
+++ src/presentation/controllers/get_document_names_controller.py
@@ -0,0 +1,28 @@
+from src.domain.use_cases.I_get_document_names import IGetDocumentNames
+from src.presentation.interface.controller_interface import ControllerInterface
+from src.presentation.http_types.http_request import HttpRequest
+from src.presentation.http_types.http_response import HttpResponse
+
+class GetDocumentNamesController(ControllerInterface):
+
+    def __init__(self, get_document_names_case:IGetDocumentNames) -> None:
+        self.__get_document_names_case = get_document_names_case
+    
+    def handle(self, http_request:HttpRequest) -> HttpResponse:
+        try:
+            body = http_request.body
+            project_id = body['project_id']
+            names = self.__get_document_names_case.names(
+                project_id=project_id
+            )
+            return HttpResponse(
+                status_code=200,
+                body={
+                    'message': 'projects_founded',
+                    'content': {
+                        'projects': names
+                    }
+                }
+            )
+        except Exception as e:
+            raise e from e
