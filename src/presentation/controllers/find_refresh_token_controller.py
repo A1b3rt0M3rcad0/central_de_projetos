694d24e53db0ba4f2bf285590675e312cca226d8
Alberto <alberto.zimba2013@gmail.com>
2025-05-09 15:11:04 -0300

    feat: find refresh token controller created

---
+++ src/presentation/controllers/find_refresh_token_controller.py
@@ -0,0 +1,30 @@
+from src.domain.use_cases.i_find_refresh_token import IFindRefreshToken
+from src.presentation.http_types.http_request import HttpRequest
+from src.presentation.http_types.http_response import HttpResponse
+from src.presentation.interface.controller_interface import ControllerInterface
+from src.domain.value_objects.cpf import CPF
+
+class FindRefreshTokenController(ControllerInterface):
+
+    def __init__(self, find_refresh_token_case:IFindRefreshToken) -> None:
+        self.__find_refresh_token_case = find_refresh_token_case
+    
+    def handle(self, http_request:HttpRequest) -> HttpResponse:
+        try:
+            body = http_request.body
+            cpf = CPF(body['cpf'])
+            result = self.__find_refresh_token_case.find(
+                user_cpf=cpf
+            )
+            return HttpResponse(
+                status_code=200,
+                body={
+                    'message': 'Refresh token founded',
+                    'content': {
+                        'cpf': result.cpf,
+                        'token': result.token
+                    }
+                }
+            )
+        except Exception as e:
+            raise e from e
