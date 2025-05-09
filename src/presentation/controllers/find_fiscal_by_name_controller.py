9a306b8fc960d3e51d0d1b7eab469008d1ae22dd
Alberto <alberto.zimba2013@gmail.com>
2025-05-09 14:42:53 -0300

    feat: find fiscal by name controller created

---
+++ src/presentation/controllers/find_fiscal_by_name_controller.py
@@ -0,0 +1,30 @@
+from src.domain.use_cases.i_find_fiscal_by_name import IFindFiscalByName
+from src.presentation.interface.controller_interface import ControllerInterface
+from src.presentation.http_types.http_request import HttpRequest
+from src.presentation.http_types.http_response import HttpResponse
+
+class FindFiscalByNameController(ControllerInterface):
+
+    def __init__(self, find_fiscal_by_name_case:IFindFiscalByName) -> None:
+        self.__find_fiscal_by_name_case = find_fiscal_by_name_case
+    
+    def handle(self, http_request:HttpRequest) -> HttpResponse:
+        try:
+            body = http_request.body
+            name = body['name']
+            result = self.__find_fiscal_by_name_case.find(
+                name=name
+            )
+            return HttpResponse(
+                status_code=200,
+                body={
+                    'message': 'Fiscal Founded',
+                    'content': {
+                        'id': result.fiscal_id,
+                        'name': result.name,
+                        'created_at': result.created_at
+                    }
+                }
+            )
+        except Exception as e:
+            raise e from e
