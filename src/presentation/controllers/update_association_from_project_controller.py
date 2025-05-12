d73fd937318ba45d8afa98cc62fa555c400b22f0
Alberto <alberto.zimba2013@gmail.com>
2025-05-12 13:52:38 -0300

    feat: update fiscal from project controller created

--- src/presentation/controllers/update_association_from_project_controller.py
+++ src/presentation/controllers/update_association_from_project_controller.py
@@ -1,4 +1,5 @@
 from src.domain.use_cases.i_update_association_from_project import IUpdateAssociationFromProject
+from src.domain.use_cases.i_create_history_project import ICreateHistoryProject
 from src.domain.value_objects.cpf import CPF
 from src.presentation.interface.controller_interface import ControllerInterface
 from src.presentation.http_types.http_request import HttpRequest
@@ -6,8 +7,12 @@ from src.presentation.http_types.http_response import HttpResponse
 
 class UpdateAssociationFromProjectController(ControllerInterface):
 
-    def __init__(self, update_association_from_project_case:IUpdateAssociationFromProject) -> None:
+    def __init__(self, 
+                 update_association_from_project_case:IUpdateAssociationFromProject,
+                 create_history_project_case:ICreateHistoryProject
+                 ) -> None:
         self.__update_association_from_project_case = update_association_from_project_case
+        self.__create_history_project_case = create_history_project_case
     
     def handle(self, http_request:HttpRequest) -> HttpResponse:
         try:
@@ -19,6 +24,11 @@ class UpdateAssociationFromProjectController(ControllerInterface):
                 cpf=cpf,
                 project_id=project_id,
                 new_cpf=new_cpf
+            )
+            self.__create_history_project_case.create(
+                project_id=project_id,
+                data_name='cpf',
+                description=f'usuario associado trocado de {cpf.value} para {new_cpf.value}'
             )
             return HttpResponse(
                 status_code=200,
--- src/presentation/controllers/update_fiscal_from_project_controller.py
+++ src/presentation/controllers/update_fiscal_from_project_controller.py
@@ -1,38 +1,38 @@
-from src.domain.use_cases.i_update_bairro_from_project import IUpdateBairroFromProject
+from src.domain.use_cases.i_update_fiscal_from_project import IUpdateFiscalFromProject
 from src.domain.use_cases.i_create_history_project import ICreateHistoryProject
 from src.presentation.interface.controller_interface import ControllerInterface
 from src.presentation.http_types.http_request import HttpRequest
 from src.presentation.http_types.http_response import HttpResponse
 
-class UpdateBairroFromProjectController(ControllerInterface):
+class UpdateFiscalFromProjectController(ControllerInterface):
 
     def __init__(self, 
-                 update_bairro_from_project_case:IUpdateBairroFromProject,
+                 update_fiscal_from_project_case:IUpdateFiscalFromProject,
                  create_history_project_case:ICreateHistoryProject
                  ) -> None:
-        self.__update_bairro_from_project_case = update_bairro_from_project_case
+        self.__update_fiscal_from_project_case = update_fiscal_from_project_case
         self.__create_history_project_case = create_history_project_case
     
     def handle(self, http_request:HttpRequest) -> HttpResponse:
         try:
             body = http_request.body
             project_id = body['project_id']
-            bairro_id = body['bairro_id']
-            new_bairro_id = body['new_project_id']
-            self.__update_bairro_from_project_case.update(
+            fiscal_id = body['fiscal_id']
+            new_fiscal_id = body['new_fiscal_id']
+            self.__update_fiscal_from_project_case.update(
                 project_id=project_id,
-                bairro_id=bairro_id,
-                new_bairro_id=new_bairro_id
+                fiscal_id=fiscal_id,
+                new_fiscal_id=new_fiscal_id
             )
             self.__create_history_project_case.create(
                 project_id=project_id,
-                data_name='Bairro',
-                description=f'Bairro do projecto trocado de {bairro_id} para {new_bairro_id}'
+                data_name='Fiscal',
+                description=f'Fiscal do projeto atualizado de {fiscal_id} para {new_fiscal_id}'
             )
             return HttpResponse(
                 status_code=200,
                 body={
-                    'message': 'Bairro updated from project'
+                    'message': 'Fiscal from project updated'
                 }
             )
         except Exception as e:
