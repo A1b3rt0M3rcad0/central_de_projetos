5f8b460d91f6b11f4c86f56849c0e319313add0c
Alberto <alberto.zimba2013@gmail.com>
2025-05-13 13:09:11 -0300

    feat: create empresa composer created

---
+++ src/main/composers/create_empresa_controller.py
@@ -0,0 +1,19 @@
+from src.data.use_cases.create_empresa import CreateEmpresa
+from src.infra.relational.repository.empresa_repository import EmpresaRepository
+from src.infra.relational.config.connection.db_connection_handler import DBConnectionHandler
+from src.infra.relational.config.connection.string_connection import StringConnection
+from src.presentation.controllers.create_empresa_controller import CreateEmpresaController
+from src.presentation.http_types.http_request import HttpRequest
+from src.presentation.http_types.http_response import HttpResponse
+from typing import Callable
+
+def create_empresa_composer() -> Callable[[HttpRequest], HttpResponse]:
+    return CreateEmpresaController(
+        CreateEmpresa(
+            EmpresaRepository(
+                DBConnectionHandler(
+                    StringConnection()
+                )
+            )
+        )
+    )
