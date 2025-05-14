b14243915237c8a63aafe08c3cd87e3c088aa681
Alberto <alberto.zimba2013@gmail.com>
2025-05-14 13:46:38 -0300

    feat: delete status composer created

---
+++ src/main/composers/delete_status_composer.py
@@ -0,0 +1,16 @@
+from src.main.config.database.db_connection_handler_factory import db_connection_handler_factory
+from src.infra.relational.repository.status_repository import StatusRepository
+from src.data.use_cases.delete_status import DeleteStatus
+from src.presentation.controllers.delete_status_controller import DeleteStatusController
+from src.presentation.http_types.http_request import HttpRequest
+from src.presentation.http_types.http_response import HttpResponse
+from typing import Callable
+
+def delete_status_composer() -> Callable[[HttpRequest], HttpResponse]:
+    return DeleteStatusController(
+        delete_status_case=DeleteStatus(
+            status_repository=StatusRepository(
+                db_connection_handler=db_connection_handler_factory()
+            )
+        )
+    ).handle
