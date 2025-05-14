36c55ea1b246e8a6e494f7be55c18f2ceb34f6fc
Alberto <alberto.zimba2013@gmail.com>
2025-05-14 16:31:50 -0300

    feat: update_project_start_date_composer create

---
+++ src/main/composers/update_project_start_date_composer.py
@@ -0,0 +1,23 @@
+from src.main.config.database.db_connection_handler_factory import db_connection_handler_factory
+from src.infra.relational.repository.project_repository import ProjectRepository
+from src.infra.relational.repository.history_project import HistoryProjectRepository
+from src.data.use_cases.create_history_project import CreateHistoryProject
+from src.data.use_cases.update_project_start_date import UpdateProjectStartDate
+from src.presentation.controllers.update_project_start_date_controller import UpdateProjectStartDateController
+from src.presentation.http_types.http_request import HttpRequest
+from src.presentation.http_types.http_response import HttpResponse
+from typing import Callable
+
+def update_project_start_date() -> Callable[[HttpRequest], HttpResponse]:
+    return UpdateProjectStartDateController(
+        create_history_project_case=CreateHistoryProject(
+            history_project_repository=HistoryProjectRepository(
+                db_connection_handler=db_connection_handler_factory()
+            )
+        ),
+        update_project_start_date_case=UpdateProjectStartDate(
+            project_repository=ProjectRepository(
+                db_connection_handler=db_connection_handler_factory()
+            )
+        )
+    ).handle
