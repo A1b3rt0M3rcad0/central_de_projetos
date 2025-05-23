8f2c66fd38b162993ecd9b98af957a5a8ab6ffd5
Alberto <alberto.zimba2013@gmail.com>
2025-05-23 09:51:30 -0300

    feat: empresa auth routes created

--- src/main/routes/api/empresa/empresa_routes.py
+++ src/main/routes/api/empresa/empresa_routes.py
@@ -15,36 +15,45 @@ from src.main.routes.api.empresa.request_format.create_empresa_format import Cre
 from src.main.routes.api.empresa.request_format.delete_empresa_format import DeleteEmpresaFormat
 from src.main.routes.api.empresa.request_format.update_empresas_composer import UpdateEmpresasFormat
 
+## Auth
+from fastapi import Security
+from src.main.routes.middleware.authorization import role_required
+from src.main.routes.middleware.insert_access_token import insert_access_token
+
 routes = APIRouter(prefix='/empresa', tags=['empresas'])
 
 @routes.get('/{empresa_name}')
-async def find_empresa(empresa_name, request:Request):
+async def find_empresa(empresa_name, request:Request, user=Security(role_required(["VEREADOR", "ADMIN", "ASSESSOR"]))):
     try:
         http_response = await request_adapter(request, find_empresa_by_exact_name_composer())
+        http_response = await insert_access_token(http_response, request.state.new_access_token)
         return response_adapter(http_response)
     except Exception as e:
         return response_adapter(error_handler(e))
 
 @routes.post('/')
-async def create_empresa(body:CreateEmpresaFormat, request:Request):
+async def create_empresa(body:CreateEmpresaFormat, request:Request, user=Security(role_required(["VEREADOR"]))):
     try:
         http_response = await request_adapter(request, create_empresa_composer())
+        http_response = await insert_access_token(http_response, request.state.new_access_token)
         return response_adapter(http_response)
     except Exception as e:
         return response_adapter(error_handler(e))
 
 @routes.delete('/')
-async def delete_empresa(body:DeleteEmpresaFormat, request:Request):
+async def delete_empresa(body:DeleteEmpresaFormat, request:Request, user=Security(role_required(["ADMIN"]))):
     try:
         http_response = await request_adapter(request, delete_empresa_composer())
+        http_response = await insert_access_token(http_response, request.state.new_access_token)
         return response_adapter(http_response)
     except Exception as e:
         return response_adapter(error_handler(e))
 
 @routes.patch('/')
-async def update_empresa(body:UpdateEmpresasFormat, request:Request):
+async def update_empresa(body:UpdateEmpresasFormat, request:Request, user=Security(role_required(["ADMIN"]))):
     try:
         http_response = await request_adapter(request, update_empresas_composer())
+        http_response = await insert_access_token(http_response, request.state.new_access_token)
         return response_adapter(http_response)
     except Exception as e:
         return response_adapter(error_handler(e))
