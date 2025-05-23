a79e6dbd95ee06c57eda8068851304ec7f31b869
Alberto <alberto.zimba2013@gmail.com>
2025-05-23 11:19:32 -0300

    feat: status, types, user and user_project routes auth add

--- src/main/routes/api/types/types_routes.py
+++ src/main/routes/api/types/types_routes.py
@@ -16,45 +16,55 @@ from src.main.routes.api.types.request_format.create_type_format import CreateTy
 from src.main.routes.api.types.request_format.delete_type_format import DeleteTypeFormat
 from src.main.routes.api.types.request_format.update_type_format import UpdateTypeFormat
 
+## Auth
+from fastapi import Security
+from src.main.routes.middleware.authorization import role_required
+from src.main.routes.middleware.insert_access_token import insert_access_token
+
 routes = APIRouter(prefix='/types', tags=['type'])
 
 @routes.post('/')
-async def create_type(body:CreateTypeFormat, request:Request):
+async def create_type(body:CreateTypeFormat, request:Request, user=Security(role_required(["ADMIN"]))):
     try:
         http_response = await request_adapter(request, create_type_composer())
+        http_response = await insert_access_token(http_response, request.state.new_access_token)
         return response_adapter(http_response)
     except Exception as e:
         return response_adapter(error_handler(e))
 
 @routes.delete('/')
-async def delete_type(body:DeleteTypeFormat, request:Request):
+async def delete_type(body:DeleteTypeFormat, request:Request, user=Security(role_required(["ADMIN"]))):
     try:
         http_response = await request_adapter(request, delete_type_composer())
+        http_response = await insert_access_token(http_response, request.state.new_access_token)
         return response_adapter(http_response)
     except Exception as e:
         return response_adapter(error_handler(e))
 
 @routes.patch('/')
-async def update_type(body:UpdateTypeFormat, request:Request):
+async def update_type(body:UpdateTypeFormat, request:Request, user=Security(role_required(["ADMIN"]))):
     try:
         http_response = await request_adapter(request, update_type_composer())
+        http_response = await insert_access_token(http_response, request.state.new_access_token)
         return response_adapter(http_response)
     except Exception as e:
         return response_adapter(error_handler(e))
 
 @routes.get('/find_by_name/{type_name}')
-async def find_type_by_exact_name(type_name:str, request:Request):
+async def find_type_by_exact_name(type_name:str, request:Request, user=Security(role_required(["ADMIN", "VEREADOR", "ASSESSOR"]))):
     try:
         request.path_params['type_name'] = type_name
         http_response = await request_adapter(request, find_type_by_exact_name_composer())
+        http_response = await insert_access_token(http_response, request.state.new_access_token)
         return response_adapter(http_response)
     except Exception as e:
         return response_adapter(error_handler(e))
 
 @routes.get('/all')
-async def find_all_types(request:Request):
+async def find_all_types(request:Request, user=Security(role_required(["ADMIN", "VEREADOR", "ASSESSOR"]))):
     try:
         http_response = await request_adapter(request, find_all_types_composer())
+        http_response = await insert_access_token(http_response, request.state.new_access_token)
         return response_adapter(http_response)
     except Exception as e:
         return response_adapter(error_handler(e))
