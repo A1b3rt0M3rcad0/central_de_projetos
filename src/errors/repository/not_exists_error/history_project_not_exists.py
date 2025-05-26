a7e68245c075e1ea037f6b873d91b576a46be3c7
Alberto <alberto.zimba2013@gmail.com>
2025-05-26 15:21:51 -0300

    refact: history project repository errors refactored

--- src/errors/repository/not_exists_error/history_project_not_exists.py
+++ src/errors/repository/not_exists_error/history_project_not_exists.py
@@ -1,10 +1,10 @@
 from src.errors.repository.__base.base_repository_error import BaseRepositoryError
 
-class ProjectIdNotExistsError(BaseRepositoryError):
+class HistoryProjectNotExists(BaseRepositoryError):
 
     def __init__(self, message:str, *args, **kwargs) -> None:
         super().__init__(
-            title='ProjectIdNotExists',
+            title=self.__class__.__name__,
             message=message,
             *args,
             **kwargs
