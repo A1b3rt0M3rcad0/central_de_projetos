e8324710a3eeb31995da04f0ac5de3566118fa63
Alberto <alberto.zimba2013@gmail.com>
2025-06-12 11:42:17 -0300

    refact: refactored update email from user

--- src/data/use_cases/update_user_email.py
+++ src/data/use_cases/update_user_email.py
@@ -3,6 +3,7 @@ from src.domain.value_objects.cpf import CPF
 from src.data.interface.i_user_repository import IUserRepository
 from src.domain.use_cases.i_update_user_email import IUpdateUserEmail
 from src.errors.use_cases.email_already_exists_error import EmailAlreadyExistsError
+from src.errors.repository.not_exists_error.user_not_exists import UserNotExists
 
 class UpdateUserEmail(IUpdateUserEmail):
 
@@ -12,19 +13,25 @@ class UpdateUserEmail(IUpdateUserEmail):
     def update(self, cpf:CPF, email:Email) -> None:
         try:
             self.__email_already_exists(email=email)
-
+        except UserNotExists:
             update_params = {'email': email.email}
             self.__user_repository.update(
                 cpf=cpf,
                 update_params=update_params
             )
+        except EmailAlreadyExistsError as e:
+            raise e from e
         except Exception as e:
             raise e from e
     
     def __email_already_exists(self, email:Email) -> None:
         try:
             existing_user = self.__user_repository.find_by_email(email=email)
-            if existing_user:
+            if existing_user.email is not None:
                 raise EmailAlreadyExistsError(message=f'Email "{email.email}" already exists')
+        except UserNotExists as e:
+            raise e from e
+        except EmailAlreadyExistsError as e:
+            raise e from e
         except Exception as e:
             raise e from e
